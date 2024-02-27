c838029b88d85119772e3a3984aca8688c79703b
Hennadii Bohdanov <HennadiiBH01.kodehode@gmail.com>
2024-02-27 11:36:45 +0100

    Add mobile menu

---
+++ index.js
@@ -0,0 +1,20 @@
+let isMenuOpen = false;
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuCloseIcon = document.getElementById('menu-close-icon');
+const mobileMenuOpenIcon = document.getElementById('menu-open-icon');
+
+const toggleMenu = () => {
+	isMenuOpen = !isMenuOpen
+	console.log(isMenuOpen)
+
+	mobileMenuBtn.classList.toggle('menu-btn-open')
+	mobileMenuBtn.classList.toggle('menu-btn-closed')
+	mobileMenu.classList.toggle('visible')
+	mobileMenu.classList.toggle('hidden')
+	mobileMenuCloseIcon.classList.toggle('visible')
+	mobileMenuCloseIcon.classList.toggle('hidden')
+	mobileMenuOpenIcon.classList.toggle('visible')
+	mobileMenuOpenIcon.classList.toggle('hidden')
+}
+
