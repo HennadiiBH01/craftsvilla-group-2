704a5f27e50999c48ccc178a5656fe64a6546ee2
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-02-29 13:36:36 +0100

    Fix mobile menu

--- script.js
+++ script.js
@@ -15,28 +15,30 @@ const toggleMenu = () => {
 	mobileMenuCloseIcon.classList.toggle('hidden')
 	mobileMenuOpenIcon.classList.toggle('visible')
 	mobileMenuOpenIcon.classList.toggle('hidden')
+
+	document.getElementsByClassName("header-mobile")[0].classList.toggle('bottom-0')
 }
 
 // Reviews
-let slideIndex = 0;
-
-function moveSlide(n) {
-	const slides = document.getElementsByClassName("carousel-slide");
-	slideIndex += n;
-	if (slideIndex >= slides.length) {slideIndex = 0}
-	if (slideIndex < 0) {slideIndex = slides.length - 1}
-	for (let i = 0; i < slides.length; i++) {
-		slides[i].style.display = "none";
-	}
-	slides[slideIndex].style.display = "flex"; // Adjust display to 'flex'
-}
-
-document.querySelector('.container-prev').addEventListener('click', function() {
-	moveSlide(-1);
-});
-
-document.querySelector('.container-next').addEventListener('click', function() {
-	moveSlide(1);
-});
-
-moveSlide(0); // Show first slide by default
+// let slideIndex = 0;
+//
+// function moveSlide(n) {
+// 	const slides = document.getElementsByClassName("carousel-slide");
+// 	slideIndex += n;
+// 	if (slideIndex >= slides.length) {slideIndex = 0}
+// 	if (slideIndex < 0) {slideIndex = slides.length - 1}
+// 	for (let i = 0; i < slides.length; i++) {
+// 		slides[i].style.display = "none";
+// 	}
+// 	slides[slideIndex].style.display = "flex"; // Adjust display to 'flex'
+// }
+//
+// document.querySelector('.container-prev').addEventListener('click', function() {
+// 	moveSlide(-1);
+// });
+//
+// document.querySelector('.container-next').addEventListener('click', function() {
+// 	moveSlide(1);
+// });
+//
+// moveSlide(0); // Show first slide by default
