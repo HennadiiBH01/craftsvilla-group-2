d77057c5552399a307cc37152d8cfc4fdb73e80a
Hennadii Bohdanov <soulstice.web@gmail.com>
2024-02-29 22:59:39 +0100

    Switch images in gallery

--- script.js
+++ script.js
@@ -4,85 +4,82 @@ const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuCloseIcon = document.getElementById('menu-close-icon');
 const mobileMenuOpenIcon = document.getElementById('menu-open-icon');
 
+// Function to toggle the mobile menu
 const toggleMenu = () => {
-	isMenuOpen = !isMenuOpen
+	isMenuOpen = !isMenuOpen;
 
-	isMenuOpen ? document.body.style.overflow = 'hidden' : document.body.style.overflow = 'auto';
+	document.body.style.overflow = isMenuOpen ? 'hidden' : 'auto';
 
-	mobileMenuBtn.classList.toggle('menu-btn-open')
-	mobileMenuBtn.classList.toggle('menu-btn-closed')
-	mobileMenu.classList.toggle('visible')
-	mobileMenu.classList.toggle('hidden')
-	mobileMenuCloseIcon.classList.toggle('visible')
-	mobileMenuCloseIcon.classList.toggle('hidden')
-	mobileMenuOpenIcon.classList.toggle('visible')
-	mobileMenuOpenIcon.classList.toggle('hidden')
+	mobileMenuBtn.classList.toggle('menu-btn-open');
+	mobileMenuBtn.classList.toggle('menu-btn-closed');
+	mobileMenu.classList.toggle('visible');
+	mobileMenu.classList.toggle('hidden');
+	mobileMenuCloseIcon.classList.toggle('visible');
+	mobileMenuCloseIcon.classList.toggle('hidden');
+	mobileMenuOpenIcon.classList.toggle('visible');
+	mobileMenuOpenIcon.classList.toggle('hidden');
 
-	document.getElementsByClassName("header-mobile")[0].classList.toggle('bottom-0')
+	document.getElementsByClassName("header-mobile")[0].classList.toggle('bottom-0');
 }
 
-//
-// Function to toggle active smile
+// Function to toggle the active smile
 function toggleActiveSmile(clickedElement) {
-	// Get all smile images
 	const smiles = document.getElementsByClassName('smile-img');
 
-	// Loop through all smiles to deactivate them
 	for (const smile of smiles) {
 		smile.src = smile.dataset.inactive;
 	}
 
-	// Set the clicked smile as active
 	clickedElement.src = clickedElement.dataset.active;
 }
 
 // Switch Reviews
-const reviewTitle = document.getElementById('review-title')
-const reviewText = document.getElementById('review-text')
-const reviewAuthor = document.getElementById('review-author')
+const reviewTitle = document.getElementById('review-title');
+const reviewText = document.getElementById('review-text');
+const reviewAuthor = document.getElementById('review-author');
 
-const reviews = [{
-	title: "Perfect kids' party",
-	text: `"Craftsvilla's painting party was a hit for my kid's birthday! Engaged kids, friendly staff, clean space. Will definitely recommend!"`,
-	author: "- David (with young children)"
-}, {
-	title: "Fun with friends",
-	text: `"Mosaic workshop with friends at Craftsvilla was amazing! Great instructor, quality kits, and tons of fun. Highly recommend!"`,
-	author: "- Sarah (group outing)"
-}, {
-	title: "Creative team bonding",
-	text: `"Our corporate team loved the watercolor workshop at Craftsvilla! Learned new skills, laughed together, and created beautiful art."`,
-	author: "- Maria (corporate team)"
-},]
-
+const reviews = [
+	{
+		title: "Perfect kids' party",
+		text: `"Craftsvilla's painting party was a hit for my kid's birthday! Engaged kids, friendly staff, clean space. Will definitely recommend!"`,
+		author: "- David (with young children)"
+	},
+	{
+		title: "Fun with friends",
+		text: `"Mosaic workshop with friends at Craftsvilla was amazing! Great instructor, quality kits, and tons of fun. Highly recommend!"`,
+		author: "- Sarah (group outing)"
+	},
+	{
+		title: "Creative team bonding",
+		text: `"Our corporate team loved the watercolor workshop at Craftsvilla! Learned new skills, laughed together, and created beautiful art."`,
+		author: "- Maria (corporate team)"
+	},
+];
 
 let currentIndex = 0;
-const fadeOutDuration = 500; // Duration of the fade-out animation in milliseconds
-const fadeInDuration = 500; // Duration of the fade-in animation in milliseconds
-const stayAtFullOpacityDuration = 2000; // Duration to stay at 100% opacity before transitioning to the next review in milliseconds
+const fadeOutDuration = 500;
+const fadeInDuration = 500;
+const stayAtFullOpacityDuration = 2000;
 const reviewChangeInterval = fadeOutDuration + fadeInDuration + stayAtFullOpacityDuration;
 
+// Function to update the displayed review
 function updateReview() {
 	const currentReview = reviews[currentIndex];
 
-	// Update text content
 	reviewTitle.textContent = currentReview.title;
 	reviewText.textContent = currentReview.text;
 	reviewAuthor.textContent = currentReview.author;
 
-	// Fade out
 	setTimeout(() => {
-		reviewTitle.style.transition = `opacity ${fadeOutDuration}ms ease-out`;
-		reviewText.style.transition = `opacity ${fadeOutDuration}ms ease-out`;
-		reviewAuthor.style.transition = `opacity ${fadeOutDuration}ms ease-out`;
-
-		reviewTitle.style.opacity = 0;
-		reviewText.style.opacity = 0;
-		reviewAuthor.style.opacity = 0;
+		// Fade out
+		[reviewTitle, reviewText, reviewAuthor].forEach(element => {
+			element.style.transition = `opacity ${fadeOutDuration}ms ease-out`;
+			element.style.opacity = 0;
+		});
 	}, 0);
 
-	// Change review while it's at 0 opacity
 	setTimeout(() => {
+		// Change review
 		currentIndex = (currentIndex + 1) % reviews.length;
 
 		// Update text content
@@ -90,15 +87,12 @@ function updateReview() {
 		reviewText.textContent = reviews[currentIndex].text;
 		reviewAuthor.textContent = reviews[currentIndex].author;
 
-		// Fade in
 		setTimeout(() => {
-			reviewTitle.style.transition = `opacity ${fadeInDuration}ms ease-in`;
-			reviewText.style.transition = `opacity ${fadeInDuration}ms ease-in`;
-			reviewAuthor.style.transition = `opacity ${fadeInDuration}ms ease-in`;
-
-			reviewTitle.style.opacity = 1;
-			reviewText.style.opacity = 1;
-			reviewAuthor.style.opacity = 1;
+			// Fade in
+			[reviewTitle, reviewText, reviewAuthor].forEach(element => {
+				element.style.transition = `opacity ${fadeInDuration}ms ease-in`;
+				element.style.opacity = 1;
+			});
 		}, 0);
 	}, fadeOutDuration);
 
@@ -106,5 +100,34 @@ function updateReview() {
 	setTimeout(updateReview, reviewChangeInterval);
 }
 
+// GALLERY
+
+const galleryContainer = document.getElementById("image");
+const imageSources = [
+	"images/Art-1.webp",
+	"images/Art-2.webp",
+	"images/Art-3.webp",
+];
+let currentImageIndex = 0;
+
+const nextImage = new Image();
+nextImage.src = imageSources[1];
+
+// Function to switch gallery images with opacity transition
+function switchImages() {
+	galleryContainer.style.transition = "background-image 0.8s ease-in-out";
+	galleryContainer.style.backgroundImage = `url(${imageSources[currentImageIndex]})`;
+	galleryContainer.style.backgroundSize = "cover";
+
+	setTimeout(() => {
+		currentImageIndex = (currentImageIndex + 1) % imageSources.length;
+		nextImage.src = imageSources[currentImageIndex];
+	}, 800);
+}
+
+// Call the switchImages function at intervals
+setInterval(switchImages, 5000);
+
 // Initial update
 updateReview();
+switchImages();
